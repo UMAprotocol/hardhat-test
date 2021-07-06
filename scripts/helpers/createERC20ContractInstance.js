5b1d22806ad16296e23e2a8bbc56e1549e15506d
Tulun <jaykiraly@gmail.com>
2021-07-06 15:52:41 -0700

    Erc20 instance.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

---
+++ scripts/helpers/createERC20ContractInstance.js
@@ -0,0 +1,26 @@
+const ethers = require("ethers");
+const VotingTokenArtifact = require("@uma/core/build/contracts/VotingToken.json");
+
+// Limited ERC-20 ABI
+const abi = [
+  "function balanceOf(address owner) view returns (uint)",
+  "function transfer(address to, uint amount)",
+  "event Transfer(address indexed from, address indexed to, uint amount)",
+];
+
+// interface Network {
+//   [key: string]: {
+//     address: string;
+//     events: object;
+//     links: object;
+//     transactionHash: string;
+//   };
+// }
+
+// Default to UMA Mainnet Contract Address.
+module.exports = function createERC20ContractInstance(signer, networkId) {
+  const artifact = VotingTokenArtifact.networks;
+  const network = artifact[networkId];
+
+  return new ethers.Contract(network.address, abi, signer);
+};
--- scripts/test.js
+++ scripts/test.js
@@ -1,32 +1,57 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
-const createVotingContractInstance = require("./helpers/createVotingContractInstance");
+const createERC20ContractInstance = require("./helpers/createERC20ContractInstance");
 
 async function main() {
+  const devAccount = "0x2210087BF0fD1C787e87d3a254F56a33D428312D";
+  const foundationAccount = "0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc";
+
   try {
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: ["0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc"],
+      params: [foundationAccount],
     });
 
-    const signer = await ethers.provider.getSigner(
-      "0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc"
-    );
+    const signer = await ethers.provider.getSigner(foundationAccount);
 
-    const vc = createVotingContractInstance(signer, "1");
-    console.log("vc", vc);
-
+    const erc20 = createERC20ContractInstance(signer, "1");
     await hre.network.provider.send("hardhat_setBalance", [
-      "0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc",
-      "0x100000000000000000",
+      foundationAccount,
+      "0x10000000000000000000000",
     ]);
 
-    const tx = await signer.sendTransaction({
-      to: "0x2210087BF0fD1C787e87d3a254F56a33D428312D",
+    const txOne = await signer.sendTransaction({
+      to: devAccount,
       value: ethers.utils.parseEther("0.01"),
     });
 
-    console.log("TX", tx);
+    const txTwo = await erc20.transfer(
+      foundationAccount,
+      ethers.utils.parseEther("20")
+    );
+
+    const balanceFA = await erc20.balanceOf(foundationAccount);
+    console.log("balance FA", balanceFA);
+    console.log("TXOne", txOne);
+    console.log("TXTwo", txTwo);
+
+    await hre.network.provider.request({
+      method: "hardhat_stopImpersonatingAccount",
+      params: [foundationAccount],
+    });
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [devAccount],
+    });
+
+    const devSigner = await ethers.provider.getSigner(devAccount);
+
+    const erc20Dev = createERC20ContractInstance(devSigner, "1");
+
+    const balance = await erc20Dev.balanceOf(devAccount);
+
+    console.log("balance", balance);
   } catch (err) {
     console.log("err", err);
   }
