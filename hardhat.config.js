53c85a571a56f1ab6502416a5426738149988b2f
Tulun <jaykiraly@gmail.com>
2022-01-28 09:53:26 -0800

    make sure it works for tom
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -51,7 +51,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 1337,
       hardfork: "london",
       gasPrice: 100,
       initialBaseFeePerGas: 1_000_000_000,
